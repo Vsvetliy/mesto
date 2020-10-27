a4ad27f7cc2ab7a400b3682673664f1d69cc568b
Vladimir Kolomeytsev <vsvetliy@gmail.com>
2020-10-27 21:36:31 +0700

    Мне кажется идеально....

--- script.js
+++ script.js
@@ -7,41 +7,30 @@ let songsContainer = document.querySelector('.profile__author_edit');
 let popupToggle = function (event) {
     event.preventDefault();
     popup.classList.toggle('popup_opened');
-}
 
-//popupToggle();
+}
 
 
 popupOpenButton.addEventListener('click', popupToggle);
+
 popupCloseButton.addEventListener('click', popupToggle);
 
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__container') // Воспользуйтесь методом querySelector()
-console.log(formElement)
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
 
-    // Находим поля формы в DOM
+let formElement = document.querySelector('.popup__container') 
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+
+    
     let nameInput = formElement.querySelector('.Popup__author');
   let jobInput = formElement.querySelector('.Popup__author-subline');
   
   document.querySelector('.profile__quote-author').textContent = nameInput.value;
  document.querySelector('.profile__quote-author-subline').textContent = jobInput.value;
   
-    // Получите значение полей из свойства value
-   
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
 }
+seveButton.addEventListener('click', popupToggle);
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-//formElement.addEventListener('submit', formSubmitHandler); 
 seveButton.addEventListener('click', formSubmitHandler); 
