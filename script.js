f9fdd82dcfc1fe266a41f4f7e9cfea2e6845b11b
Vladimir Kolomeytsev <vsvetliy@gmail.com>
2020-10-27 23:42:48 +0700

    Мне кажется идеально......Страдания

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let popup = document.querySelector('.popup');
-let popupOpenButton = document.querySelector('.profile__edit_button');
-let popupCloseButton = popup.querySelector('.popup__close_button');
+let popupOpenButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = popup.querySelector('.popup__close-button');
 let page = document.querySelector('.page');
-let seveButton = document.querySelector('.popup__Submit_Button')
-let songsContainer = document.querySelector('.profile__author_edit');
+let seveButton = document.querySelector('.popup__Submit-Button')
+let songsContainer = document.querySelector('.profile__author-edit');
 let popupToggle = function (event) {
     event.preventDefault();
     popup.classList.toggle('popup_opened');
@@ -24,7 +24,7 @@ function formSubmitHandler (evt) {
     evt.preventDefault(); 
 
     
-    let nameInput = formElement.querySelector('.Popup__author');
+    let nameInput = formElement.querySelector('.popup__container-author');
   let jobInput = formElement.querySelector('.Popup__author-subline');
   
   document.querySelector('.profile__quote-author').textContent = nameInput.value;
