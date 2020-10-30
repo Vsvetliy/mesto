d19cb611d82d53a8e94494276af594d77f3d2851
Vladimir Kolomeytsev <vsvetliy@gmail.com>
2020-10-31 02:19:36 +0700

    JS

--- script/script.js
+++ script/script.js
@@ -9,17 +9,31 @@ let nameInput = formElement.querySelector('.popup__input_author');
 let jobInput = formElement.querySelector('.popup__input_job');
 let nameProfile = document.querySelector('.profile__quote-author');
 let jobProfile = document.querySelector('.profile__quote-author-subline');
-let popupToggle = function (event) {
+//let popupToggle = function (event) {
     //event.preventDefault();
-    popup.classList.toggle('popup_opened');
+   //popup.classList.toggle('popup_opened');
 
+//}
+
+
+//popupOpenButton.addEventListener('click', popupToggle);
+
+//popupCloseButton.addEventListener('click', popupclose);
+
+
+function popupOpen (evt) {
+    popup.classList.add('popup_opened');
+    console.log(1)
+    nameInput.value = nameProfile.textContent;
+    console.log(nameProfile.textContent)
+    jobInput.value = jobProfile.textContent;
+    console.log(jobProfile.textContent)
 }
 
 
-popupOpenButton.addEventListener('click', popupToggle);
-
-popupCloseButton.addEventListener('click', popupToggle);
-
+function popupClose (evt) {
+      popup.classList.remove('popup_opened');
+}
 
 
 
@@ -27,14 +41,11 @@ popupCloseButton.addEventListener('click', popupToggle);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-
-    
-    
-  
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
-  
+    popup.classList.remove('popup_opened');
 }
-seveButton.addEventListener('click', popupToggle);
 
-seveButton.addEventListener('click', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler);
+popupCloseButton.addEventListener('click', popupClose); 
+popupOpenButton.addEventListener('click', popupOpen); 
