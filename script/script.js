5d6c5cac4b1e45490490b368c96dccba98754729
Vladimir Kolomeytsev <vsvetliy@gmail.com>
2021-01-23 17:47:08 +0700

    test 1

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 let popup = document.querySelector('.popup');
-let popupAddForm = document.querySelector('.popup__add-form');
+let popupAddForm = document.querySelector('.popup-add-form');
 let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = popup.querySelector('.popup__close-button');
 let popupCloseButton2 = popupAddForm.querySelector('.popup__close-button');
@@ -12,10 +12,13 @@ let nameInput = formElement.querySelector('.popup__input_name_author');
 let jobInput = formElement.querySelector('.popup__input_name_job');
 let nameProfile = document.querySelector('.profile__quote-author');
 let jobProfile = document.querySelector('.profile__quote-author-subline');
-let popupAddButton = document.querySelector('.popup__add-button');
+let popupAddButton = document.querySelector('.popup-add-form__button');
 let titleInput = document.querySelector('.popup__input_title');
 let linkInput = document.querySelector('.popup__input_link');
 let elements = document.querySelector('.elements');
+let popupImg = document.querySelector('.popup-img');
+let popupImgImages = popupImg.querySelector('.popup-img__images');
+let popupImgText = popupImg.querySelector('.popup-img__text');
 
 
 
@@ -51,12 +54,15 @@ const initialCards = [
   let elementTemplate = document.querySelector('.element-template').content;
 
 function render() {
-  //функция пройди по массиву и выполни renderItem
+  
   initialCards.forEach(renderItem);
 }
 
+
+
+
 function renderItem(element) {
-    let templateCards = elementTemplate.cloneNode(true); //клонируем шаблон
+    let templateCards = elementTemplate.cloneNode(true); 
     let elementDeleteButton = templateCards.querySelector('.element__delete-button').addEventListener('click', handleDelete);
     let elementlikebutton = templateCards.querySelector('.element__like-button').addEventListener('click', function (evt) {
       evt.target.classList.toggle('element__like-button_activ');
@@ -64,22 +70,32 @@ function renderItem(element) {
 
     templateCards.querySelector('.element__text').textContent = element.name; 
     templateCards.querySelector('.element__images').src = element.link;
-    
-    elements.append(templateCards);  //вставляем в elements наш шаблон.
-    console.log(elementlikebutton)
+    templateCards.querySelector('.element__images').addEventListener('click', function (evt) {
+      popupImg.classList.add('popup-img_opened');
+      popupImgImages.src = element.link;
+      popupImgText.textContent = element.name;
+    });
+    elements.append(templateCards);  
   }
+
 
 
 
   function formAddElement (evt) {
    
-    let templateCards = elementTemplate.cloneNode(true); //клонируем шаблон
+    let templateCards = elementTemplate.cloneNode(true); 
     let elementDeleteButton = templateCards.querySelector('.element__delete-button').addEventListener('click', handleDelete);
-    let elementlikebutton = templateCards.querySelector('.element__like-button').addEventListener('click', handleLike);
+    let elementlikebutton = templateCards.querySelector('.element__like-button').addEventListener('click', function (evt) {
+      evt.target.classList.toggle('element__like-button_activ');
+    });
 
     templateCards.querySelector('.element__text').textContent = titleInput.value; 
     templateCards.querySelector('.element__images').src = linkInput.value;
-    elements.prepend(templateCards);  //вставляем в elements наш шаблон.
+    templateCards.querySelector('.element__images').addEventListener('click', function (evt) {
+      popupImg.classList.add('popup-img_opened');
+
+    });
+    elements.prepend(templateCards);  
    popupClose ()
    
 }
@@ -88,35 +104,6 @@ function handleDelete(evt) {
 	evt.target.closest('.element').remove();
 
 }
-
-
-
-//  elements.innerHTML = templateElement;
-//function renderItem(element) {
-//	const htmlElement = elementTemplate.cloneNode(true);
-//	htmlElement.querySelector('.item__text').innerText = text;
- // elements.append(templateCards)
-//	setListeners(htmlElement);
-	//list.appendChild(htmlElement);
-//}
-
-//function setListeners(element) {
-	//element.querySelector('.delete').addEventListener('click', handleDelete);
-	//element.querySelector('.edit').addEventListener('click', handleEdit);
-	//element.querySelector('.duplicate').addEventListener('click', handleDuplicate);
-//}
-
-
-
-//function handleLike(evt) {
-  //element.target.closest('.element__like-button').classList.add('element__like-button_activ');
- //elementlikebutton.classList.add('element__like-button_activ');
-    
-//}
-
-
-
-
 
 
 
@@ -136,29 +123,39 @@ profileaddbutton.addEventListener('click', popupOpen2)
 
 
 function popupOpen (evt) {
-    popup.classList.add('popup_opened');
-    nameInput.value = nameProfile.textContent;
-    jobInput.value = jobProfile.textContent;
+  popup.classList.add('popup_opened');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
 
 
 function popupClose (evt) {
+  popup.classList.remove('popup_opened');
   popupAddForm.classList.remove('popup_opened');
-      popup.classList.remove('popup_opened');
-      
+
 }
 
 
 function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    nameProfile.textContent = nameInput.value;
-    jobProfile.textContent = jobInput.value;
-    popupClose (evt)
-    
+  evt.preventDefault(); 
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  popupClose (evt)
+  
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 popupAddButton.addEventListener('click', formAddElement);
-popupCloseButton.addEventListener('click', popupClose); 
+popupCloseButton.addEventListener('click', popupClose);
 popupCloseButton2.addEventListener('click', popupClose); 
-popupOpenButton.addEventListener('click', popupOpen) 
+popupOpenButton.addEventListener('click', popupOpen); 
+
+popupImg.classList.remove('popup-img_opened');
+
+
+popupImg.querySelector('.popup__close-button').addEventListener('click', function (evt) {
+  popupImg.classList.remove('popup-img_opened');
+});
+popupImg.addEventListener('click', function (evt) {
+  popupImg.classList.remove('popup-img_opened');
+});
