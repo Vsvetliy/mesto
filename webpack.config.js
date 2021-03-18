33802d980595185b08ad482c43750acf74306ce5
Vladimir Kolomeytsev <vsvetliy@gmail.com>
2021-03-19 02:25:01 +0700

    ПР8-2

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
 
@@ -12,33 +12,32 @@ module.exports = {
     },
     mode: 'development',
     devServer: {
-        contentBase: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-        compress: true, // это ускорит загрузку в режиме разработки
-        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+        contentBase: path.resolve(__dirname, './dist'), 
+        compress: true, 
+        port: 8080, 
     
-        open: true // сайт будет открываться сам при запуске npm run dev
+        open: true 
       },
       module: {
-        rules: [ // rules — это массив правил
-          // добавим в него объект правил для бабеля
+        rules: [ 
+          
           {
-            // регулярное выражение, которое ищет все js файлы
+            
             test: /\.js$/,
-            // при обработке этих файлов нужно использовать babel-loader
+            
             use: 'babel-loader',
-            // исключает папку node_modules, файлы в ней обрабатывать не нужно
+            
             exclude: '/node_modules/'
           },
           {
-            // регулярное выражение, которое ищет все файлы с такими расширениями
+            
             test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
             type: 'asset/resource'
           },
           {
-            // применять это правило только к CSS-файлам
+           
             test: /\.css$/,
-            // при обработке этих файлов нужно использовать
-            // MiniCssExtractPlugin.loader и css-loader
+            
             use: [MiniCssExtractPlugin.loader, {
               loader: 'css-loader',
               options: { importLoaders: 1 }
@@ -49,9 +48,9 @@ module.exports = {
       },
       plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html' // путь к файлу index.html
+            template: './src/index.html'
           }),
-          new CleanWebpackPlugin(), // использовали плагин
+          new CleanWebpackPlugin(), 
           new MiniCssExtractPlugin()
-      ] // добавьте массив
+      ] 
 };
